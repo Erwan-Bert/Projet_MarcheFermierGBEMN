6df77cb3d290f91433fcccce8618599880904b6b
CORRE Morgan <c17001656@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-12 08:20:13 +0100

    Création du main

--- src/main/java/marche/traitement/MainMarche.java
+++ src/main/java/marche/traitement/MainMarche.java
@@ -1,11 +1,12 @@
-package marche.traitement.Produit;
+package marche.traitement;
 
+import marche.traitement.Produit.Produit;
 import marche.traitement.Produit.ProduitsLaitiers.Lait;
 import marche.traitement.Produit.ProduitsLaitiers.ProduitsLaitier;
 
 import java.util.Date;
 
-public class TestProduit {
+public class MainMarche {
     public static void main(String[] args) {
         Produit produit1 = new Lait(5,new Date(2020,4,11),"Litre");
         produit1.valider();
