a7644dfa9a8c5ad16509df36d0aa2c0552ce175f
c17001656 <corre.morgan@etu.univ-amu.fr>
2018-12-12 10:19:03 +0100

    création d'un main fonctionnel

--- src/main/java/marche/traitement/MainMarche.java
+++ src/main/java/marche/traitement/MainMarche.java
@@ -1,20 +1,57 @@
 package marche.traitement;
 
-import marche.traitement.Produit.Produit;
-import marche.traitement.Produit.ProduitsLaitiers.Lait;
-import marche.traitement.Produit.ProduitsLaitiers.ProduitsLaitier;
-import marche.traitement.Produit.Viande.Cochon;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import marche.traitement.UnitedeProduction.UniteDeProduction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainMarche {
-    public static void main(String[] args) throws ParseException {
-        Produit produit1 = new Lait(5,"13/12/2018","Litres");
-        Produit produit2 = new Cochon(10,"11/12/2018","Kilogrames");
-        produit1.valider();
-        produit2.valider();
+    public static void main(String[] args)
+    {
+        String temp ="";
+        int avancement = 1;
+        String produit = "";
+        Integer quantite = 0;
+        String peremption = "";
+        while (!temp.equals("fini"))
+        {
+            temp = menu(avancement);
+            if (avancement == 1)
+                produit = temp;
+            else if (avancement == 2)
+                quantite = Integer.parseInt(temp);
+            else if (avancement == 3)
+                peremption = temp;
+            else if (avancement == 4 && temp.equals("autre produit"))
+            {
+                System.out.println(UniteDeProduction.fabriquer(produit, quantite, peremption));
+                avancement = 0;
+            }
+            avancement += 1;
+        }
+        System.out.println(UniteDeProduction.fabriquer(produit, quantite, peremption));
+    }
+
+    private static String menu(int avancement) {
+
+        String choix = "";
+        if (avancement == 1)
+            System.out.println("Entrez le nom de votre produit");
+        else if (avancement == 2)
+            System.out.println("Entrez la quantite produite (en litre ou kilo selon votre produit)");
+        else if (avancement == 3)
+            System.out.println("Entrez la date de péremption du produit sous la forme jj/mm/aaaa");
+        else if (avancement == 4)
+            System.out.println("Entrez fini si vous avez fini ou autre produit si vous avez d'autres produits à enregistrer");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            choix = in.readLine();
+        } catch (IOException e) {
+            System.out.println("Problème de saisie");
+        }
+        return choix;
     }
 }
