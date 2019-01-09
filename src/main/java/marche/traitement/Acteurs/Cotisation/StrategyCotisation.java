f82c808e36089e6ce7789c3a8e94261aa9c70d30
nathan-vignal <nathan.vignal@etu.univ-amu.fr>
2019-01-09 16:29:12 +0100

    supression de l'utilisation de getclass dans venderuAcheteur, developpement de Strategy cotisation comme changeant un coût de base

--- src/main/java/marche/traitement/Acteurs/Cotisation/StrategyCotisation.java
+++ src/main/java/marche/traitement/Acteurs/Cotisation/StrategyCotisation.java
@@ -4,6 +4,7 @@ package marche.traitement.Acteurs.Cotisation;
 	 * Permet de calculer la valeure de la cotisation selon la strategie
      */
 public interface StrategyCotisation {
+    int coutCotisation = 300;
 
     int calculCotisation();
 }
