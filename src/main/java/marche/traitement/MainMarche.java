83fae01dd824f494af5137ba91d75c8b4a45bc3f
nathan vignal <37573225+nathan-vignal@users.noreply.github.com>
2019-01-09 23:26:45 +0100

    suppression de commentaire

--- src/main/java/marche/traitement/MainMarche.java
+++ src/main/java/marche/traitement/MainMarche.java
@@ -5,21 +5,6 @@ import marche.affichage.AffichageTerminal;
 import marche.traitement.Initialisation.Initialisation;
 
 public class MainMarche {
-    /*public static void main(String[] args)
-    {
-        LocalDate peremption = LocalDate.of(2015, 8, 11);
-        System.out.println(peremption.plusDays(1));
-        UniteDeProductionApiculteur uniteDeProductionApiculteur = new UniteDeProductionApiculteur("Les miellistes");
-        UniteDeProductionLaitier uniteDeProductionLaitier = new UniteDeProductionLaitier("Les laitiers");
-        ProducteurLaitier prod = new ProducteurLaitier(400,"tpo",562);
-        ProducteurBio producteurLaitier = new ProducteurBio(new ProducteurLaitier(400,"tpo",562));
-
-
-        uniteDeProductionLaitier.ajouterMembre(producteurLaitier);
-        uniteDeProductionLaitier.ajouterMembre(new ProducteurDeViande(544445454, "Robert", 1));
-
-
-    }*/
 
     public static void main(String[] args)
     {
